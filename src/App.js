95e2c59c55b6a2b5eeb0fdb3ab87a25e6a8ed8f0
Sam <sammohanty04@gmail.com>
2023-06-12 21:33:58 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -26,6 +26,19 @@ export default class App extends Component {
     return (
       <>
         <div className='App '>
+          <input type='search'
+          className='search-box'
+          placeholder='Search Names'
+          onChange={(e)=>{console.log(e.target.value)
+
+            const searchName = e.target.value.toLocaleLowerCase()
+            const filteredNames = this.state.monstors.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchName)
+            })
+            this.setState(()=>{
+              return {monstors: filteredNames}
+            })
+          }} />
           {this.state.monstors.map((monster)=>{
             return <p>{monster.name}</p>
           })}
